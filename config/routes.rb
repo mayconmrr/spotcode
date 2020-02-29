2e8700a997f21906bf651d6d2cebd00dbaf0e92d
mayconmrr <maycon.mrr@gmail.com>
2020-02-29 12:01:27 -0700

    Add favorite option to album and artist

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,17 @@ Rails.application.routes.draw do
     root to: 'dashboard#index', as: :authenticated_root
     resources :search, only: [:index, :new], as: :searches
     resources :categories, only: :show
-    resources :artists, only: :show
     resources :albums, only: :show do
+      post '/favorite', to: 'favorites#create', on: :member, defaults: { format: :js, favoritable_type: 'Album' }
+      delete '/favorite', to: 'favorites#destroy', on: :member, defaults: { format: :js, favoritable_type: 'Album' }
       resources :recently_heards, only: :create
     end
+
+    resources :artists, only: :show do
+      post '/favorite', to: 'favorites#create', on: :member, defaults: { format: :js, favoritable_type: 'Artist' }
+      delete '/favorite', to: 'favorites#destroy', on: :member, defaults: { format: :js, favoritable_type: 'Artist' }
+    end
+
     resources :songs, only: [] do
       post '/favorite', to: 'favorites#create', on: :member, defaults: { format: :js, favoritable_type: 'Song' }
       delete '/favorite', to: 'favorites#destroy', on: :member, defaults: { format: :js, favoritable_type: 'Song' }
